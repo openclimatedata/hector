ba295a7530caca0600d38b2ab8ca7e04f1c4cbce
Sven Willner <sven.willner@pik-potsdam.de>
2017-01-31 17:48:28 +0100

    turned off logger in carbon-cycle-solver

--- source/core/carbon-cycle-solver.cpp
+++ source/core/carbon-cycle-solver.cpp
@@ -42,7 +42,7 @@ CarbonCycleSolver::~CarbonCycleSolver()
 //------------------------------------------------------------------------------
 // documentation is inherited
 void CarbonCycleSolver::init( Core* coreptr ) {
-    logger.open( getComponentName(), true, Logger::NOTICE );
+    logger.open( getComponentName(), false, Logger::NOTICE );
     H_LOG( logger, Logger::DEBUG ) << getComponentName() << " initialized." << std::endl;
     
     core = coreptr;
